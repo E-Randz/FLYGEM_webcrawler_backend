8b5388bb7efcfbab99dd0b7d128114b36fde8e0c
Emma Randall <eerandall1@gmail.com>
2019-02-12 14:55:37 +0000

    create if statement for post method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  console.log(req);
 })
 
 server.listen(9090, () => console.log('server listening on port 9090'));
