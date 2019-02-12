eeb5f95a8bc5f8a0dab70cf290daa2b6a7877843
Emma <38498590+E-Randz@users.noreply.github.com>
2019-02-12 15:17:52 +0000

    Server (#1)
    * create server.js file
    
    * create http server within server.js
    
    * create if statement for post method
    
    * create regex for req url
    
    * create invokation of req.on function to receive the body of the post request
    
    * remove body from req destructure in server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  
+  if (method === 'POST') {
+    let body = ''
+    if(/\/api\/webcrawler/.test(url)){
+      req.on('data', chunk => {
+        body += chunk.toString()
+      })
+      req.on('end', () => {
+        res.end('ok');
+      })
+    }
+  }
+})
+
+server.listen(9090, () => console.log('server listening on port 9090'));
