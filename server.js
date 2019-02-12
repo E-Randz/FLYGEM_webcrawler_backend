eec367bff17211842e1cfb102a07dc4752daad42
Emma Randall <eerandall1@gmail.com>
2019-02-12 14:57:34 +0000

    create regex for req url

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const { url, method } = req;
+  
+  if (method === 'POST') {
+    if(/\/api\/webcrawler/.test(url))
+  }
 })
 
 server.listen(9090, () => console.log('server listening on port 9090'));
